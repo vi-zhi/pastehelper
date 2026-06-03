a3770f08202290ffbfc9edec2698062222dc0d20
vizhi <2720561950@qq.com>
2026-06-03 21:17:52 +0800

    feat:init

---
+++ src/main/java/com/vizhi/pastehelper/PastehelperApplication.java
@@ -0,0 +1,13 @@
+package com.vizhi.pastehelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PastehelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PastehelperApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vizhi/pastehelper/PastehelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vizhi.pastehelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PastehelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
